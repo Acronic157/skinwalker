cf05ab96831b15333700917290ee0e546c2a8733
Acronic157 <65405040+Acronic157@users.noreply.github.com>
2026-02-06 22:48:53 +0100

    quick save - idle animation player

--- scripts/menus/outro/outro.gd
+++ scripts/menus/outro/outro.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
+@onready var lamp: AnimatedSprite2D = $lamp
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_lamp_animation_finished() -> void:
+	Globals.next_level.emit()
+
+func _on_dialogue_ended():
+		lamp.play("default")

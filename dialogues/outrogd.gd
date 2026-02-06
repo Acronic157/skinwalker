a71f8e9108e281b6352a34b97be22671b28c0147
Acronic157 <65405040+Acronic157@users.noreply.github.com>
2026-02-06 23:31:26 +0100

    Outro screen

---
+++ dialogues/outrogd.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@export var dialogue_resource: DialogueResource
+# then
+@export var dialogue_start: String = "start"
+
+const Balloon = preload("res://dialogues/ballons/balloon.tscn")
+@onready var lamp: AnimatedSprite2D = $"../../lamp"
+
+# or
+#var dialogue_line = await resource.get_next_dialogue_line("start")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var balloon: Node = Balloon.instantiate()
+	get_tree().current_scene.add_child.call_deferred(balloon)
+	balloon.call_deferred("start", dialogue_resource, dialogue_start)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_dialogue_ended(ressource) -> void:
+	lamp.play("default")
+
+func _on_lamp_animation_finished() -> void:
+	print("anim ended")
+	Levelmanager.current_level += 1
+	Levelmanager._unlock_level(Levelmanager.current_level)
+	var level_to_load: String = Levelmanager._load_level(Levelmanager.current_level)
+	if level_to_load == "":
+		return
+	get_tree().call_deferred("change_scene_to_file", level_to_load)
